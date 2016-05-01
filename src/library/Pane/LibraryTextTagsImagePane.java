fa20b74d28def18d07e50ae19ae68b8a0e66df5d
Tristan Beckley <tristanbeckley@yahoo.com>
2016-05-01 00:54:03 -0400

    Alpha 1 is officially complete. Sighs, yay. I have a hotkey, a search system (can only search one tag at a time right now). But it's my tag system, and my search system. Woo.

--- src/library/Pane/LibraryTextTagsImagePane.java
+++ src/library/Pane/LibraryTextTagsImagePane.java
@@ -1,9 +1,13 @@
 package library.Pane;
 
+import java.awt.Desktop;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,9 +20,9 @@ import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
 
-
 import library.File.LibraryFile;
 import library.TextField.LibrarySearchBar;
+
 import xml.XML;
 
 public class LibraryTextTagsImagePane extends JSplitPane
@@ -61,6 +65,32 @@ public class LibraryTextTagsImagePane extends JSplitPane
 				int width = imageIcon.getIconWidth()/3;
 				image.labelImage.setIcon(new ImageIcon(imageIcon.getImage().getScaledInstance(width, -1, Image.SCALE_FAST)));		
 			}
+		});
+		
+		selectFiles.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyTyped(KeyEvent e) {}
+			
+			@Override
+			public void keyReleased(KeyEvent e) 
+			{
+				if (KeyEvent.getKeyText(e.getKeyCode()).equals("O"))
+				{
+					LibraryFile selectedLibraryFileKey = search.getSortedCatalog().get(selectFiles.getSelectedIndex());
+					try 
+					{
+						Desktop.getDesktop().open(new File(selectedLibraryFileKey.path));
+					} 
+					catch (IOException e1) 
+					{
+						e1.printStackTrace();
+					}
+				}
+			}
+			
+			@Override
+			public void keyPressed(KeyEvent e) {}
 		});
 		
 		JSplitPane imageTags = new JSplitPane(JSplitPane.VERTICAL_SPLIT,image,tags);
